118792c5baab43683bf1184c5dee55d3a2a6a8fb
Abhyuday Tripathi <tripathi.abhyuday2010@gmail.com>
2021-02-03 15:27:58 +0530

    Initial commit

--- App.js
+++ App.js
@@ -9,3 +9,27 @@ Webcam.set({
 });
 
 Webcam.attach('#webcamContainer');
+
+const takeSnapshot = () => {
+  Webcam.snap(dataUri => {
+    document.querySelector(
+      '#outputImage'
+    ).innerHTML = `<img src=${dataUri} id="capturedImage"/>`;
+  });
+};
+
+const modelLoaded = () => console.log('Model Loaded!');
+
+const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+const gotResult = (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    document.querySelector('#object').innerText = result[0].label;
+  }
+};
+
+const check = () => {
+  classifier.classify(document.querySelector('#capturedImage'), gotResult);
+};
